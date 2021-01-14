ea6fbf37eaaf15398ee7d7a08470fbda2377a372
Reddy Tintaya <aaabeeelooon@gmail.com>
2021-01-13 22:11:54 -0400

    Calculadora con material UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { TextField, Button } from "@material-ui/core";
+
+import "./App.css";
 
 function App() {
+  const [numero1, setNumero1] = useState();
+  const [numero2, setNumero2] = useState();
+  const [resultado, setResultado] = useState();
+
+  const handleChangeNum1 = (e) => {
+    setNumero1(e.target.value);
+  };
+
+  const handleChangeNum2 = (e) => {
+    setNumero2(e.target.value);
+  };
+
+  const sumar = () => {
+    setResultado(Number(numero1) + Number(numero2));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <TextField
+          type="number"
+          name="number1"
+          label="Ingrese Número 1"
+          value={numero1}
+          onChange={handleChangeNum1}
+        />
+      </div>
+
+      <div>
+        <TextField
+          type="number"
+          name="number2"
+          value={numero2}
+          label="Ingrese Número 2"
+          onChange={handleChangeNum2}
+        />
+      </div>
+
+      <Button variant="contained" color="secondary" onClick={sumar}>
+        Sumar
+      </Button>
+
+      <div> Resultado: {resultado}</div>
     </div>
   );
 }
